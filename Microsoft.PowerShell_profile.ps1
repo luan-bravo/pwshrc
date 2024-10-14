1e0dda2e40c71ca0f6deb741a317f28810713ca8
luan.brav0 <luan.melo.bravo10@gmail.com>
2024-10-13 21:23:43 -0300

    couple aliases added

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,12 @@
 oh-my-posh init pwsh --config "~/Documents/PowerShell/gruvbox.omp.json" | Invoke-Expression
 
 Set-Alias -Name c -Value clear
+Set-Alias -Name q -Value exit
 Set-Alias -Name xa -Value ls
 Set-Alias -Name py -Value python3
 Set-Alias -Name python -Value python3
+Set-Alias -Name ino -Value arduino-cli
+Set-Alias -Name espt -Value "python -m esptool"
 
 function gs {git status $args}
 function ga {git add $args}
@@ -14,4 +17,4 @@ function gcm {git commit -m $args}
 function gb {git branch $args}
 function .. {cd ..}
 function ~ {cd ~}
-function which {param($Exe) (Get-Command $Exe).path} 
+function which {param($Exe) (Get-Command $Exe).path}
