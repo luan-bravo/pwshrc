759602f2166becfe29cb5d6ad2b36db5218fa5bc
luan.brav0 <luan.melo.bravo10@gmail.com>
2025-04-03 19:58:16 -0300

    Add git diff aliases | fix 'gaa' | change indent for line

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,35 +12,47 @@ Set-Alias -Name ino -Value arduino-cli
 Set-Alias -Name espt -Value "python -m esptool"
 
 function x {eza -l -h -n -s='type' --icons $args}
-    function ll {eza -l -h -n -s='type' --icons $args}
+function ll {eza -l -h -n -s='type' --icons $args}
 function xa {eza -l -h -n -s='type' --icons -a $args}
-    function l {eza -l -h -n -s='type' --icons -a $args}
+function l {eza -l -h -n -s='type' --icons -a $args}
 function xt {eza -l -h -n -s='type' --icons -T $args}
 function xta {eza -l -h -n -s='type' --icons -a -T $args}
 
 Remove-Item -Path Alias:g* -Force # Remove default aliases to allow overwriting
 function gs {git status $args}
+
+function gd {git diff $args}
+function gds {git diff --staged $args}
+
 function ga {git add $args}
-    function gaa {git add . $args}
-    function gap {git add -p $args}
+function gaa {git add -a $args}
+function gap {git add -p $args}
+
 function gc {git commit $args}
-    function gcv {git commit -v $args}
-    function gcm {git commit -m $args}
-    function gcam {git commit -am $args}
-    function gcma {git commit -am $args}
+function gcv {git commit -v $args}
+function gcm {git commit -m $args}
+function gcam {git commit -am $args}
+function gcma {git commit -am $args}
+
 function grs {git restore $args}
-    function grst {git restore --staged $args}
+function grst {git restore --staged $args}
+
 function gp {git push $args}
+
 function gf {git fetch $args}
-    function gfo {git fetch origin $args}
+function gfo {git fetch origin $args}
+
 function gl {git pull $args}
-    function glr {git pull --rebase $args}
-    function glra {git pull --rebase $args}
+function glr {git pull --rebase $args}
+function glra {git pull --rebase $args}
+
 function gb {git branch $args}
+
 function glog {git log --oneline --all --graph $args}
-    function gloga {git log --oneline --all --graph $args}
+function gloga {git log --oneline --all --graph $args}
+
 function gsw {git switch $args}
-    function gswc {git switch -c $args}
+function gswc {git switch -c $args}
 
 function .. {cd ..}
 function ~ {cd ~}
